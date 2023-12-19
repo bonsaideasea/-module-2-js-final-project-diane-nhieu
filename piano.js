00b14d05783772674d64ea49c5183ec841e3d6f2
bonsaideasea <bonsaideasea@gmail.com>
2023-12-19 14:26:21 -0800

    before i switch up some js syntax

--- piano.js
+++ piano.js
@@ -23,6 +23,9 @@ pianoNotes.forEach(key => {
     key.addEventListener('click', () => playTune(key.dataset.key))
 })
 
+const changeVolume = (e) => {
+    audio.volume = e.target.value;
+}
 
 const showNotes = () => {
     pianoNotes.forEach(key => key.classList.toggle('hide'));
@@ -35,3 +38,30 @@ const pressedKey = (e) => {
 keyCheckbox.addEventListener('input', showNotes);
 volumeSlider.addEventListener('input', changeVolume);
 document.addEventListener('keydown', pressedKey);
+
+ 
+function getMic(){ 
+    var captureBtn = document.getElementById('captureBtn'); 
+    captureBtn.onclick = function(){   // when the button capture is clicked...
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      const audioContext = new AudioContext()
+
+      navigator.mediaDevices.getUserMedia({audio:true}) // this begins to gather microphone input
+          .then(function(stream){
+            const source = audioContext.createMediaStreamSource(stream); // the mic input is stored as audio data in a new node called "source"
+            const pitchAnalyzer = audioContext.createAnalyser(); // a new node now can display audio time, frequency data, and can create data visualizations
+            source.connect(pitchAnalyzer);  //connects source to pitch analyzer and stores data as an object
+            let audioData = pitchAnalyzer.fftSize = 512; // passing in 512 bins or number of samples
+            const bufferLength = pitchAnalyzer.frequencyBinCount; // contains data values from pitchAnalyzer. 1/2 of fftSize so we get 256 animated bars
+            const frequencyData = new Uint8Array(bufferLength); // converts data into special type of array, UInt8Array, something about 8 integers
+            let frequencyDataArray = [];
+            for (let i = 0; i <= frequencyData[100]; i++) {
+              frequencyDataArray.push(frequencyData[i]);
+            }
+          })
+          .catch(function(err) {
+            console.error(`you got an error: ${err}`);
+          }); 
+      };
+    };
+getMic();
